3c1c55abdfe3aa04517aed0aee665bb4c0959297
Richard Harrington <rwharrington87@gmail.com>
2016-04-18 20:14:22 -0400

    add component and provider to root render function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import { Provider } from 'react-redux';
 import Calculator from './components/Calculator';
+import initializeStore from './flux/store';
+let store = initializeStore();
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(<Calculator />, document.getElementById('root'));
+  render((
+    <Provider store={store}>
+      <Calculator />
+    </Provider>
+  ), document.getElementById('root'));
 });
